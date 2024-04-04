f557440a840a0ae95da2e5a5761b52d4629bb195
tolaoguntunde <50242126+tolaoguntunde@users.noreply.github.com>
2024-04-03 21:23:59 -0400

    let

--- test/app.test.js
+++ test/app.test.js
@@ -1,20 +1,22 @@
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let expect = chai.expect;
-chai.use(chaiHttp);
+// Dynamically import the necessary ES modules
+let chai, app;
 
-let app = require('../script.js'); // Adjust the path as necessary
+before(async function() {
+  chai = await import('chai');
+  const chaiHttp = await import('chai-http');
+  app = await import('../app.js'); // Adjust the path to where your Express app is exported
+
+  chai.use(chaiHttp.default);
+});
 
 describe('Application routes', function() {
   describe('GET /', function() {
-    it('should return the index.html file', function(done) {
-      chai.request(app)
-        .get('/')
-        .end(function(err, res) {
-          expect(res).to.have.status(200);
-          expect(res).to.be.html;
-          done();
-        });
+    it('should return the index.html file', async function() {
+      const res = await chai.request(app.default).get('/');
+      chai.expect(res).to.have.status(200);
+      chai.expect(res).to.be.html;
     });
   });
+
+  // Add your POST test here, adjusting as necessary for async/await syntax
 });
